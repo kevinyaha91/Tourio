83d41cc471a20e45ab96d2031abd98d126d9d2c1
NamNguyen52 <nam.nguyen52@gmail.com>
2015-01-17 12:20:42 -0800

    user show works now

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,15 +2,12 @@ class ContractsController < ApplicationController
 
 	def new
 		user = params[:user]
-		user_id = user.to_i
-		@user = User.where(id: user_id).to_a
+		@user = User.where(id: user).to_a
 	end
 
 	def single_person
 		user = User.find(params[:user_id])
 		user_id = user.id
-		render :js => "window.location = #{new_contract_path(:user => 'user_id').to_json}"
-	end
-
-	
+		render :js => "window.location = #{new_contract_path(:user => user_id).to_json}"
+	end	
 end
